dab79ee1cdead55aeb463c2033997925a4da8193
Akshat Gupta <akshatgupta1507@gmail.com>
2024-06-04 19:45:49 +0530

    Add Autoclose

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 async function printing(e) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        let new_text = document.createElement('p');
-        new_text.innerHTML = `>> ${e} <div class="container">
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let new_text = document.createElement("p");
+      new_text.innerHTML = `>> ${e} <div class="container">
           <div></div>
           <div></div>
           <div></div>
           <div></div>
           <div></div>
         </div><br>`;
-        document.querySelector(".custom").appendChild(new_text);
-        resolve(new_text); // Resolve the promise with the new text element
-      }, Math.ceil(Math.random()*3000));
-    });
-  }
-  
-  async function main() {
-    await printing("Initializing Hacking");
-    await printing("Reading Your Files");
-    await printing("Password Files Detected");
-    await printing("Sending all passwords and personal files to the server");
-    await printing("Cleaning up");
-  }
-  
-  main();
-  
+      document.querySelector(".custom").appendChild(new_text);
+      resolve(new_text); // Resolve the promise with the new text element
+    }, 5000);
+  });
+}
+
+async function main() {
+  await printing("Initializing Hacking");
+  await printing("Reading Your Files");
+  await printing("Password Files Detected");
+  await printing("Sending all passwords and personal files to the server");
+  await printing("Cleaning up");
+  setTimeout(() => {
+    window.close();
+  }, 3000);
+}
+
+main();
